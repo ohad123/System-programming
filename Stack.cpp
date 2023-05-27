6740d306a8e903735df9f163185892b9f2d35bd8
Ohad Adi <79912542+ohad123@users.noreply.github.com>
2023-05-27 17:35:27 +0300

    Add files via upload
    o.o.p in c++
    Practice in: class - function constructs, destroys, public, private
    building Vending machine 
    using ADT- stack

---
+++ Stack.cpp
@@ -0,0 +1,77 @@
+/* Assignment C++: 1
+   Author: Ohad Adi, ID: 315636753
+           Tslil Sardam, ID:209488717
+*/
+
+
+#include "Stack.h"
+#include <climits>
+#include <iostream>
+using namespace std;
+
+Stack::Stack(){
+    this->top = NULL;
+}
+
+void Stack::push(int num)
+{
+    StackNode* NewSN = new StackNode;
+    StackNode* temp;
+    temp = top;
+    top = NewSN;
+    top->update_data(num);
+    top->update_next(temp);
+}
+
+void Stack::pop()
+{
+    if (isEmpty())
+        cout << "Stack empty" << endl;
+    else
+    {
+        if (top->get_next() == NULL)	/* there is only one object in the stack */
+        {
+            cout << "Removing " << top->get_data() << endl;
+            delete top;
+            top = NULL;
+        }
+        else
+        {
+            StackNode* sec = top->get_next();
+            cout << "Removing " << top->get_data() << endl;
+            delete top;
+            top = sec;
+        }
+    }
+}
+
+bool Stack::isEmpty()
+{
+    if (top == NULL)
+        return true;
+    else
+        return false;
+}
+
+int Stack::peek()
+{
+    if (isEmpty()) {
+        cout << "The first element is: " << INT_MIN << endl;
+        return INT_MIN;
+    }
+    else
+    {
+        cout << "The first element is: " << top->get_data() << endl;
+        return top->get_data();
+    }
+
+}
+
+
+Stack::~Stack() {
+    while (!isEmpty()) {
+        StackNode *sec = top->get_next();
+        delete top;
+        top = sec;
+    }
+}
---
+++ main.cpp
@@ -0,0 +1,17 @@
+/* Assignment C++: 1
+   Author: Ohad Adi, ID: 315636753
+           Tslil Sardam, ID:209488717
+*/
+
+#include "Menu.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    //simply creating a menu instance, then calling mainMenu(). the rest is inside mainMenu().
+    Menu a;
+    a.mainMenu();
+    return 0;
+}
+
